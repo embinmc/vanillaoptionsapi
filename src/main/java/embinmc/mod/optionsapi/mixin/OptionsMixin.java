8ca29fbe339df53eb1fb2e76a7dbda59103fbdd9
Embin <113154360+embinmc@users.noreply.github.com>
2026-03-06 11:45:52 -0800

    Update OptionsMixin.java

--- src/main/java/embinmc/mod/optionsapi/mixin/OptionsMixin.java
+++ src/main/java/embinmc/mod/optionsapi/mixin/OptionsMixin.java
@@ -10,14 +10,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Options.class)
 public abstract class OptionsMixin {
 
-	@Inject(method = "processOptions", at = @At(
-            value = "INVOKE",
-            ordinal = 4, // completely arbitrary
-            target = "Lnet/minecraft/client/Options$FieldAccess;process(Ljava/lang/String;Lnet/minecraft/client/OptionInstance;)V"
-    ))
+	@Inject(method = "processOptions", at = @At(value = "HEAD"))
 	private void process(final Options.FieldAccess access, CallbackInfo info) {
         VanillaOptionsAPI.forEachOption((identifier, optionInstanceSupplier) -> {
-            String formattedKey = identifier.toLanguageKey();
+            String formattedKey = identifier.toLanguageKey().replace('/', '.');
             access.process(formattedKey, optionInstanceSupplier.get());
         });
 	}
